5c1822b99f62c16802f82bb1a9a1081a80573c7a
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-09-29 08:12:41 +0100

    Dodgems implements ITic

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,20 @@
 package attractions;
+import people.Visitor;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+
+public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+    }
+    public double defaultPrice(){
+        return 4.5;
+    }
+    public double priceFor(Visitor visitor){
+        if(visitor.getAge()<=12){
+            return 0.5*defaultPrice();
+        }
+        else return defaultPrice();
     }
 }
